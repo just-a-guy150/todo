8a51509dd811b0a53ad898c1ed9cc9c7941e407b
just-a-guy150 <dimonmig301@gmail.com>
2026-02-28 13:06:57 +0200

    v1.12

---
+++ server.js
@@ -0,0 +1,88 @@
+const express = require('express')
+import mysql from 'mysql2/promise'
+import dotenv from 'dotenv'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+dotenv.config()
+
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10, // 67
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/register', async (req, res) => {
+    const { login, email, password } = req.body
+    try {
+        let result = await pool.query(
+            `SELECT * FROM Users WHERE login = ? OR email = ?`,
+            [login, email]
+        )
+
+        if (result[0].length > 0) {
+            return res.status(400).send({
+                error: "Error registering user"
+            })
+        }
+
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password, salt)
+
+        await pool.query(
+            `INSERT INTO Users (login, email, password) VALUES (?, ?, ?)`,
+            [login, email, hashedPassword]
+        )
+        res.status(201).send({ message: "User registered successfully" })
+    } catch (error) {
+        res.status(500).send("Error registering user.I wonder why did it go wrong?")
+    }
+})
+
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const users = await pool.query(
+            `SELECT * FROM Users WHERE email = ?`,
+            [email]
+        )
+
+        if (users.length === 0) {
+            return res
+                .status(401)
+                .send({ error: "Invalid email or password. сам ты инвалид" })
+        }
+
+        const user = users[0]
+
+        const isValidPassword = await bcrypt.compare(password, user.password)
+        if (!isValidPassword) {
+            return res
+                .status(401)
+                .send({ error: "Invalid email or password. ох как же меня заклебали эти инвалиды" })
+        }
+
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET, { expiresIn: '1h' })
+        res.status(200).send({ token })
+
+
+
+    } catch {
+        res.status(500).send({ error: "Error logging in." })
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
